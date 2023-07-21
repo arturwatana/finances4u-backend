4e7db9c93b68c62c33105ee1af3467540b74415d
Artur Watanabe <arturwatanabe@gmail.com>
2023-07-20 22:41:14 -0300

    fix:vercel deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { SignInModule } from './modules/signIn/signIn.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
+import { PasswordBcryptHash } from './utils/hashPassword/passwordHash.bcrypt.implementation';
 
 @Module({
   imports: [UserModule, SignInModule, TransactionsModule],
   controllers: [],
   providers: [
+    PasswordBcryptHash,
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
